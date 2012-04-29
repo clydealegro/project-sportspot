a640780b032dd1969c1c442c07c8c6eac0a6f1cd
Clyde Alegro <clyde.alegro@gmail.com>
2012-04-29 17:18:20 +0800

    setup html5boilerplate on the layout template

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -7,8 +7,13 @@
     <link rel="shortcut icon" href="/favicon.ico" />
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
+    
+    <script src="js/libs/modernizr-2.5.3.min.js"></script>
   </head>
   <body>
+  
+      <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+      
   
         <?php require_once("_header.php"); ?>
       
@@ -17,6 +22,24 @@
         </div>
         
         <?php require_once("_footer.php"); ?>
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+          <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+        
+          <script src="js/plugins.js"></script>
+          <script src="js/script.js"></script>
+        
+        <script type="text/javascript" src="http://use.typekit.com/jym5bou.js"></script>
+        <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+        
+        
+        <script>
+          var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+          (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+          g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+          s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+        
         
   </body>
 </html>
