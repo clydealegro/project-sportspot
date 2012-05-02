8c869320ec785f650ff3092f7d7b4875a94da507
Patrick Ruther Dumilon <patrickruther@ymail.com>
2012-05-02 19:34:33 +0800

    added routes to

--- apps/frontend/templates/_header.php
+++ apps/frontend/templates/_header.php
@@ -1,5 +1,5 @@
 <header>
     <h1>Company Name</h1>
-    <a href="">Sign Up</a>
-    <a href="">Sign In</a>
+    <a href="<?php echo url_for('register') ?>">Sign Up</a>
+    <a href="<?php echo url_for('login') ?>">Sign In</a>
 </header>
