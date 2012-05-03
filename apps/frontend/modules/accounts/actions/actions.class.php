e3de658b72c86e3bf7617a1fe2b3291afacb3c50
Clyde Alegro <clyde.alegro@gmail.com>
2012-05-03 21:03:46 +0800

    added profile, account, and search modules

---
+++ apps/frontend/modules/accounts/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * accounts actions.
+ *
+ * @package    project-sportspot
+ * @subpackage accounts
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class accountsActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
