a03751ade1a039ce1fa2cb6e74cc827ab9e1d6cf
Clyde Alegro <clyde.alegro@gmail.com>
2012-05-03 21:00:50 +0800

    added search section on the homepage

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -1,5 +1,13 @@
 <div class="container">
     <div class="row">
         <h2>Welcome to the directory of sports and fitness venues in the Philippines.</h2>
+        <div class="search">
+            <select name="" id="">
+                <option value="">Location</option>
+                <option value="">Sport</option>
+            </select>
+            <input type="text" class="text searchbox" value="Type in your search here" />
+            <input type="submit" class="submit button" value="Search" />
+        </div>
     </div>
 </div>
