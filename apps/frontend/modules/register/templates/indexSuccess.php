3218ef4af7ec661ae82ed5935ce1b7032f6b7d15
farly <vftaboada@gmail.com>
2012-05-11 13:33:11 +0800

    added modal box for resetting password.

--- apps/frontend/modules/register/templates/indexSuccess.php
+++ apps/frontend/modules/register/templates/indexSuccess.php
@@ -1,24 +1,24 @@
+<script src="/js/jquery-ui/js/jquery-ui-1.8.20.custom.min.js"></script> 
+
 <h1>Register</h1>
 <?php if ($sf_user->hasFlash('notice')): ?>
   <div ><?php echo $sf_user->getFlash('notice') ?></div>
 <?php endif ?><br/>
+
 <?php include_partial('form', array('form' => $form)) ?>
 <a href="javascript:void(0)">Reset Password</a>
 <div id="forgotPassword" style="display:none">
-	<form method="post" action="<php echo url_for('@accoount-request-reset-password')>">
-		<input name="email" type="text" placeHolder="sample@mail.com">
-		<input type="submit" value="reset password">
-	</form>
+	<div clas="">
+		<form method="post" action="<?php echo url_for('@account-request-reset-password')?>">
+			<label>Input Email</label>
+			<input name="email" type="text" placeHolder="sample@mail.com" />
+			<input type="submit" value="reset password" />
+		</form>
+	</div>
 </div>
 
 <script type="text/javascript">
-	$(document).ready(function(){
-		$('div#forgotPassword').dialog({
-			autoOpen: true,
-			width: 420
-		});
-	});
 	$('a').click(function(){
-		alert('yeah jquery');
+		$('div#forgotPassword').dialog();
 	});
 </script>
