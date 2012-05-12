c2d14a5452b6452e3dd022046458cdbb26ef0e46
farly <vftaboada@gmail.com>
2012-05-12 15:41:29 +0800

    added token generator and parser. requires crypt.php

---
+++ lib/utility/token/TokenGenerator.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class TokenGenerator
+{
+	public function __construct()
+	{
+		return $this;
+	}
+
+	public function getToken($id,$type)
+	{
+		$key = md5(uniqid().time());
+		$token = 'id='.$id.'&type='.$type.'&key='.$key;
+
+		$crypt = new Crypt();
+		$token = $crypt->encrypt($token);
+
+		return $token; 
+	}
+
+	public function parseToken($token)
+	{
+    $crypt = new Crypt();
+    $param = $crypt->decrypt($token);
+
+    $tempParam = array_unique(explode('&',$param));
+    $params = array();
+
+    foreach($tempParam as $t){
+      $temp = explode('=',$t);
+      $params[$temp[0]] = $temp[1];
+    }
+
+    return $params;
+	}
+}
